fb989adb60ecf98697b35fc359c560b245e718ee
Xiaoyi Liu <lxy878@gmail.com>
2020-03-12 13:58:33 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,3 +1,11 @@
 class Appointment
+  @@all = []
+  attr_reader
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
   
 end
